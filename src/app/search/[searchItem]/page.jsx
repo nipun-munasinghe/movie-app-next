80d95e807bd9956d1a1693699cb655f9d71aa6cc
nipun-munasinghe <nipunlakmal884@gmail.com>
2025-06-19 12:21:56 +0530

    Refactor SearchPage component to fetch and display movie search results

--- src/app/search/[searchItem]/page.jsx
+++ src/app/search/[searchItem]/page.jsx
@@ -1,9 +1,19 @@
+import Results from '@/app/components/Results';
 import React from 'react'
 
-export default function page({params}) {
-    const searchTerm = params.searchTerm;
-    
+export default async function SearchPage({ params }) {
+  const seachTerm = params.searchTerm;
+  const res = await fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${seachTerm}&language=en-US&page=1&include_adult=false`
+  );
+  const data = await res.json();
+  const results = data.results;
   return (
-    <div>page</div>
-  )
+    <div>
+      {results &&
+        results.length === <h1 className='text-center pt-6'>No results found</h1>
+      }
+      {results && <Results results={results} />}
+    </div>
+  );
 }
